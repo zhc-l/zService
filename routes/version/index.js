c4405ea1a064ce9491b8f924d86dd271d3aac971
周浩臣 <2898178554@qq.com>
2023-07-24 10:12:44 +0800

    git忽略规则

---
+++ routes/version/index.js
@@ -0,0 +1,14 @@
+var express = require('express')
+const http = require('http')
+var router = express.Router()
+
+/* GET home page. */
+router.get('/api/version', function (req, res, next) {
+  //  res.render('index', { title: 'Express' })
+  // http.get('http://localhost:3000/api/version', resp => {
+  //   console.log(resp)
+  // })
+  res.send('1.0.0')
+})
+
+module.exports = router
