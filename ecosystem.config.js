00c9a9b688e7d9ea3b592624010e74a9b9661756
zhc <2898178554@qq.com>
2023-09-08 12:35:06 +0800

    main分之

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,7 +11,7 @@ module.exports = {
       user: 'root',
       host: ['116.62.114.25'],
       port: '22',
-      ref: 'origin/master',
+      ref: 'origin/main',
       repo: 'git@github.com:zhc-l/zService.git',
       path: '/zService',
       'post-deploy': 'npm run install && pm2 reload ecosystem.config.js --env production',
